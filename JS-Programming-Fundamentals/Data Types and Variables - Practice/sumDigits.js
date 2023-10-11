0a61d2c7b685f986a6fc798a05e45d9ed06a9012
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-10-11 15:18:09 +0300

    Exercises

--- JS-Programming-Fundamentals/Data Types and Variables - Practice/sumDigits.js
+++ JS-Programming-Fundamentals/Data Types and Variables - Practice/sumDigits.js
@@ -1,1 +1,12 @@
+function sumDigits(num) {
+    num = String(num);
+    let sum = 0;
 
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+
+    console.log(sum);
+}
+
+sumDigits(245678);
