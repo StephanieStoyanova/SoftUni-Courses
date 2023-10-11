4131d6186b9bcd836a4a94cd2e937b1400c3002b
Stephanie Stoyanova <146414846+StephanieStoyanova@users.noreply.github.com>
2023-10-11 12:53:56 +0300

    Delete JS-Programming-Fundamentals/Basic-Syntax-Conditional-Statements-And-Loops-Practice directory

--- JS-Programming-Fundamentals/Basic-Syntax-Conditional-Statements-And-Loops-Practice/triangleOfNumbers.js
+++
@@ -1,9 +0,0 @@
-function triangleOfNumbers(maxRow) {
-    for (let num = 1; num <= maxRow; num++) {    
-        console.log(`${num} `.repeat(num));
-    }
-}
-
-triangleOfNumbers (3);
-triangleOfNumbers (5);
-triangleOfNumbers (6);
