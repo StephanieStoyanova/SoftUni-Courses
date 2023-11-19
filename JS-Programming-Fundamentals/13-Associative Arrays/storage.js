273cfbeb75748eb0ada01fe7d65e411d5bfc576f
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-19 22:11:41 +0200

    Exercise

---
+++ JS-Programming-Fundamentals/13-Associative Arrays/storage.js
@@ -0,0 +1,31 @@
+function storage(arr) {
+    let map = new Map();
+
+    for (let item of arr) {
+        let tokens = item.split(" ");
+        let product = tokens[0];
+        let quantity = Number(tokens[1]);
+
+        if (!map.has(product)) {
+            map.set(product, +quantity);
+        } else {
+            let currQuantity = map.get(product);
+            let newQuantity = currQuantity += quantity;
+            map.set(product, newQuantity);
+        }
+    }
+
+    for (let kvp of map) {
+        console.log(`${kvp[0]} -> ${kvp[1]}`);
+    }
+}
+
+storage(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40']);
+
+storage(['apple 50',
+'apple 61',
+'coffee 115',
+'coffee 40']);
