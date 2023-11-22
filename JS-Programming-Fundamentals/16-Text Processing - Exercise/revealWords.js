4dd4a0da0cc00d031cf8d13755bcb58da177bbca
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-22 19:48:25 +0200

    Exercise

--- JS-Programming-Fundamentals/16-Text Processing - Exercise/revealWords.js
+++ JS-Programming-Fundamentals/16-Text Processing - Exercise/revealWords.js
@@ -1,5 +1,20 @@
-function revealWords() {
+function revealWords(words, text) {
+    words = words.split(", ");
+    text = text.split(" ");
 
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].includes("*")) {
+            let wordFromTextLength = text[i].length;
+
+            for (let j = 0; j < words.length; j++) {
+                if (words[j].length === wordFromTextLength) {
+                    text[i] = words[j];
+                }
+            }
+        }
+    }
+
+    console.log(text.join(" "));
 }
 
 revealWords('great', 'softuni is ***** place for learning new programming languages');
