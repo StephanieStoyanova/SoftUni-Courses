31caafc70d96d17945ae75f6c5c079c364b38d71
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-23 23:21:09 +0200

    Exercise

--- JS-Programming-Fundamentals/16-Text Processing - Exercise/replaceRepeatingChars.js
+++ JS-Programming-Fundamentals/16-Text Processing - Exercise/replaceRepeatingChars.js
@@ -1,5 +1,15 @@
-function replaceRepeatingChars() {
+function replaceRepeatingChars(string) {
+    let characters = string.split('');
 
+    let result = '';
+
+    for (let i = 0; i < characters.length; i++) {
+        if (characters[i] !== characters[i + 1]) {
+            result += characters[i];
+        }
+    }
+
+    console.log(result);
 }
 
 replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa');
