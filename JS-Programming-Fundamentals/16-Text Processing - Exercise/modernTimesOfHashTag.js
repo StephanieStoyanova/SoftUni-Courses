975bdf0d58b5bb13285b26a25b149af2f5dfabea
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-23 22:14:34 +0200

    Exercise

--- JS-Programming-Fundamentals/16-Text Processing - Exercise/modernTimesOfHashTag.js
+++ JS-Programming-Fundamentals/16-Text Processing - Exercise/modernTimesOfHashTag.js
@@ -1,5 +1,14 @@
-function modernTimesOfHashTag() {
-    
+function modernTimesOfHashTag(text) {
+    let words = text.split(" ");
+
+    for (let word of words) {
+        if (word.startsWith("#")) {
+            let specialWord = word.slice(1);
+            if (/^[A-Za-z]+$/.test(specialWord)) {
+            console.log(specialWord);
+            }
+        }
+    }
 }
 
 modernTimesOfHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia');
