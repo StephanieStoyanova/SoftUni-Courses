e84df973398a3bf96638bcf16da25562e7fdb271
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-23 23:15:08 +0200

    Exercise

--- JS-Programming-Fundamentals/16-Text Processing - Exercise/stringSubstring.js
+++ JS-Programming-Fundamentals/16-Text Processing - Exercise/stringSubstring.js
@@ -1,5 +1,12 @@
-function stringSubstring() {
+function stringSubstring(searchedWord, text) {
+    text = text.toLowerCase().split(" ");
+    searchedWord = searchedWord.toLowerCase();
 
+    if (text.includes(searchedWord)) {
+        console.log(searchedWord);
+    } else {
+        console.log(`${searchedWord} not found!`);
+    }
 }
 
 stringSubstring('javascript', 'JavaScript is the best programming language');
