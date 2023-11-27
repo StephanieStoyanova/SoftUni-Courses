b307e3a0c10f47bba28f1a5ff3995a66f8846d76
Stephanie Stoyanova <stoyanovastefani02@gmail.com>
2023-11-27 16:34:57 +0200

    Exercise

--- JS-Programming-Fundamentals/18-Regular Expressions - Exercise/furniture.js
+++ JS-Programming-Fundamentals/18-Regular Expressions - Exercise/furniture.js
@@ -1,4 +1,33 @@
-function furniture() {
+function furniture(arr) {
+    let items = [];
+    let totalPrice = 0;
+
+    let pattern = />>(?<name>[A-Z][A-Za-z]+)<<(?<price>\d+\.?\d+)!(?<qty>\d+)/;
+
+    let command = arr.shift();
+
+    while (command != "Purchase") {
+        let match = command.match(pattern);
+
+        if (match) {
+            let { name, price, qty } = match.groups;
+
+            let furniturePrice = Number(qty) * Number(price);
+
+            items.push(name);
+            totalPrice += furniturePrice;
+        }
+
+        command = arr.shift();
+    }
+
+    console.log("Bought furniture:");
+
+    if (items.length > 0) {
+        console.log(items.join("\n"));
+    }
+
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
 
 }
 
